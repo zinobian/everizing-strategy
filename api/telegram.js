3d8a4ea7885eb90cdce427d3217e60caa440def7
zinobian <byun8302@gmail.com>
2026-08-03 22:04:41 +0900

    Refactor Telegram message handling and request options

--- api/telegram.js
+++ api/telegram.js
@@ -7,9 +7,11 @@ module.exports = async (req, res) => {
   const token = '8808573863:AAGaoZo_1PbW53UObChFlreOUTeOA1nV1WM';
   const chatId = '-1004325585686';
   
-  const message = '🚨 테스트 메시지 - 이게 보이나요?';
+  // 요청에서 보낸 메시지 받기
+  const { type, data } = req.body;
+  const message = data?.message || '테스트 메시지';
   
-  const data = JSON.stringify({
+  const postData = JSON.stringify({
     chat_id: chatId,
     text: message,
     parse_mode: 'HTML'
@@ -21,22 +23,22 @@ module.exports = async (req, res) => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': data.length
+      'Content-Length': postData.length
     }
   };
 
-  const req_tg = https.request(options, (res_tg) => {
+  const telegramReq = https.request(options, (telegramRes) => {
     let body = '';
-    res_tg.on('data', chunk => body += chunk);
-    res_tg.on('end', () => {
+    telegramRes.on('data', chunk => body += chunk);
+    telegramRes.on('end', () => {
       res.status(200).json({ ok: true, telegram_response: body });
     });
   });
 
-  req_tg.on('error', (err) => {
+  telegramReq.on('error', (err) => {
     res.status(500).json({ error: err.message });
   });
 
-  req_tg.write(data);
-  req_tg.end();
+  telegramReq.write(postData);
+  telegramReq.end();
 };
