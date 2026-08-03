f4b0bbe91353bca2f9bec3b77d4d8bac2ec83638
zinobian <byun8302@gmail.com>
2026-08-03 20:09:31 +0900

    Hardcode Telegram token and chat ID
    Replaced environment variables with hardcoded Telegram token and chat ID.

--- api/telegram.js
+++ api/telegram.js
@@ -3,8 +3,8 @@ const https = require('https');
 
 // 텔레그램 메시지 전송
 async function sendTelegramMessage(message) {
-  const token = process.env.TELEGRAM_TOKEN;
-  const chatId = process.env.TELEGRAM_CHAT_ID;
+const token = '8747099869:AAEha3cJQ1Fxufs T0hfCQs1zM617kbXuicg';
+const chatId = '-5309587192';
 
   const data = JSON.stringify({
     chat_id: chatId,
